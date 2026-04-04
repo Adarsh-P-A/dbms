435934b690175b6ca1b587a32393b82ba3341a88
Beezzz08 <keerthanamarrapu@gmail.com>
2026-04-05 01:04:37 +0530

    added image preview

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+   
+   //Sidebar code 
     const sidebar = document.querySelector('.sidebar');
     const mainContent = document.querySelector('.main-content');
     const closeBtn = document.querySelector('.close-btn');
@@ -20,4 +22,45 @@ document.addEventListener('DOMContentLoaded', () => {
         sidebar.classList.remove('sidebar-hidden');
         mainContent.classList.remove('full-width');
     });
+
+  // Image Preview
+    const imageInput = document.getElementById("imageInput");
+    const preview = document.getElementById("preview");
+
+    imageInput.addEventListener("change", function () {
+       const file = this.files[0];
+
+       if (file) {
+           const reader = new FileReader();
+
+           reader.onload = function () {
+              preview.src = reader.result;
+              preview.style.display = "block";
+           };
+
+        reader.readAsDataURL(file);
+       }
+   });
+
+
+   // Form Submit
+   const form = document.getElementById("reportForm");
+
+   form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+     const data = {
+        title: document.getElementById("title").value,
+        category: document.getElementById("category").value,
+        type: document.getElementById("type").value,
+        visibility: document.getElementById("visibility").value,
+        location: document.getElementById("location").value,
+        date: document.getElementById("date").value,
+        description: document.getElementById("description").value,
+        image: imageInput.files[0] ? imageInput.files[0].name : null
+      };
+
+    console.log("Form Data:", data);
+    alert("Report submitted (not saved yet)");
+   });
 });
